413194e644802903dc893c3b0c46f4c4d8ffe8fe
HunterKTF <jorgericardo98@hotmail.com>
2024-10-21 16:23:21 -0600

    fixed chart size bug

--- src/components/app/overview.jsx
+++ src/components/app/overview.jsx
@@ -8,8 +8,8 @@ import { dummyKPIs } from "@/lib/dummyKPIs";
 
 export default function Overview () {
   return (
-    <div className={"h-full flex flex-col justify-between items-center"}>
-      <div className={"flex h-[22%] w-full"}>
+    <div className={"h-full flex flex-col items-center"}>
+      <div className={"flex h-[28%] w-full"}>
         <div className="w-full grid grid-cols-4 gap-4">
           {
             dummyKPIs.map((data) => {
@@ -18,13 +18,13 @@ export default function Overview () {
           }
         </div>
       </div>
-      <div className={"flex h-[74%] w-full"}>
+      <div className={"h-full w-full mt-4"}>
         <div className="w-full grid grid-cols-7 gap-4">
           <div className="col-span-4 border border-black rounded-xl">
-            <OverviewChart />
+            <OverviewChart/>
           </div>
           <div className="col-span-3 border border-black rounded-xl">
-            <MetricsTable />
+            <MetricsTable/>
           </div>
         </div>
       </div>
