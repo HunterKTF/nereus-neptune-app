2ec1e7d45255807882b1b9655d092c8595920e09
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2024-10-28 17:51:47 -0600

    fixed sentry project name bug

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ export default withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "nereus-ai",
-project: "javascript-nextjs",
+project: "nereus-neptune-app",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
